65123d944115d51b9e3821974b0efd8133c69f7c
LorenzoBondan <lbbondan@gmail.com>
2023-03-13 15:57:49 -0300

    backend project created

---
+++ backend/src/main/java/lorenzo/projects/soccerleague/SoccerleagueApplication.java
@@ -0,0 +1,13 @@
+package lorenzo.projects.soccerleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerleagueApplication.class, args);
+	}
+
+}
