56a3bf11464093a6aaddf65e6df10f9df4773b4d
LorenzoBondan <lbbondan@gmail.com>
2023-04-22 11:49:33 -0300

    update some styles and changed some words at description

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -7,8 +7,8 @@ const Home = () => {
         <div className="home-container">
                 
             <div className='home-content-container'>
-                <h1>THE BRAZILIAN LEAGUE <img src="https://em-content.zobj.net/thumbs/160/apple/325/flag-brazil_1f1e7-1f1f7.png" alt="" /></h1>
-                <p>Check out all teams, stadiums and players of the first division of the <strong>toughest</strong> soccer league in the world</p>
+                <h1>THE BRAZILIAN LEAGUE SIMULATOR <img src="https://em-content.zobj.net/thumbs/160/apple/325/flag-brazil_1f1e7-1f1f7.png" alt="" /></h1>
+                <p>Simulate the result of matches and check out all the teams, stadiums and players of the first division of the <strong>toughest</strong> soccer league in the world</p>
                 <div className='home-button-zone'>
                     <Link to={`/teams`}>
                         <button className='btn btn-primary btn-home'><span>JOIN IN</span></button>
